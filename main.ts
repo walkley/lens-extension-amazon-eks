9dee58ff88564a6aac5660dd5a5700117e7a3452
Walkley He <walkleyhe@gmail.com>
2021-06-02 10:30:19 +0800

    Add Amazon EKS Cluster menu item

--- main.ts
+++ main.ts
@@ -8,4 +8,15 @@ export default class EKSExtensionMain extends LensMainExtension {
   onDeactivate() {
     console.log('lens-extension-amazon-eks de-activated');
   }
+
+  appMenus = [
+    {
+      parentId: "file",
+      label: "Add Amazon EKS Cluster",
+      after: ["Add Cluster"],
+      click: () => {
+        this.navigate("/add-eks-cluster");
+      }
+    }
+  ]
 }
